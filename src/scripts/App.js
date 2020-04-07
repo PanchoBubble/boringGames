4b7d24761a9034220e331959dcdda1446f2bbb13
Juan <juan.deluca@bubblebpm.com>
2020-04-07 19:53:21 +0100

    app

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Navbar from './containers/NavBar';
-import Singup from './containers/forms/Singup';
 import {
     BrowserRouter as Router,
     Switch,
@@ -9,23 +7,12 @@ import {
 
 export default function App() {
     return (
-        <div>
-            <Router>
-                <Navbar />
-                <div className="container pt-3">
-                    <Switch>
-                        <Route path="/singup">
-                            <Singup />
-                        </Route>
-                        <Route path="/about">
-                            <h2>About</h2>
-                        </Route>
-                        <Route path="/">
-                            <h1>Home</h1>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <h1>Home</h1>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
